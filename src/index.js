ea373d48b96c1e10e2aab2dd1a4a143570501aca
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-03-11 23:27:57 +0300

    add compose and api for index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import reviews from "./mocks/reviews.js";
 import {
-  createStore
+  createStore,
+  applyMiddleware,
+  compose,
 } from "redux";
 import {
-  Provider
+  Provider,
 } from "react-redux";
+import thunk from 'redux-thunk';
 import {
   reducer
 } from "./reducers/reducer.js";
+import {
+  createAPI
+} from './api.js';
+
+const api = createAPI();
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    )
 );
 
 ReactDOM.render(

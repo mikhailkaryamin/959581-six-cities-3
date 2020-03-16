f9d754ae6cc502ec8572b49a697be2976851fa3f
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-03-16 21:17:07 +0500

    add components sign-in and reducer user

---
+++ src/reducer/user/user.js
@@ -0,0 +1,53 @@
+import {
+  extend
+} from '../../utils.js';
+
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const ActionType = {
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => {
+    return {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: status,
+    };
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+  }
+
+  return state;
+};
+
+const Operation = {
+  checkAuth: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+      .then(() => {
+        dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+      });
+  }
+};
+
+export {
+  ActionCreator,
+  ActionType,
+  reducer,
+  AuthorizationStatus,
+  Operation,
+};

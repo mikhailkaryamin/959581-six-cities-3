9db3442fc11f86915a96cda972b2247e69c926a6
Mikhail Karyamin <46441939+mikhailkaryamin@users.noreply.github.com>
2020-09-16 22:29:46 +0500

    Update app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -73,7 +73,7 @@ const FavoritesWrapped = withErrorMessage(Favorites);
 const LoginWrapped = withErrorMessage(Login);
 const MainWrapped = withErrorMessage(Main);
 const PropertyWrapped = withErrorMessage(Property);
-const BASE_NAME = "/959581-six-cities-3";
+const BASE_NAME = "959581-six-cities-3";
 
 const App: React.FC<Props> = (props: Props) => {
   const {
